8ecb2131dc0e1a8f79fee8e5b50e4c20dd9234af
brownhr <brownhr314@gmail.com>
2022-05-04 15:45:00 -0400

    Update arima.R

--- R/arima.R
+++ R/arima.R
@@ -1,36 +1,7 @@
 library(forecast)
 library(dplyr)
 
-ar <- auto.arima(
-  y = AirPassengers
-)
+elec <- fma::uselec
 
-
-DAX <- EuStockMarkets[,"DAX"]
-
-autoplot(DAX)
-
-DAX_dc <- DAX %>% 
-  mstl() %>% 
-  seasadj()
-
-autoplot(DAX_dc)
-
-AirPassengers %>%
-  mstl(
-    # s.window = "periodic",
-    # robust = TRUE
-  ) %>%
-  seasadj() %>%
-  autoplot()
-air_dc <- decompose(AirPassengers)
-
-rem <- remainder(air_dc)
-ses <- seasonal(air_dc)
-1 - (var(rem)/var(rem + ses))
-
-
-DAX_arima <- Arima(DAX, )
-DAX_dc %>% 
-  forecast(model = DAX_arima) %>% 
-  autoplot()
+elec_ets <- ets(elec)
+fc <- forecast(elec_ets)
