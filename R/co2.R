70f97c2c760cd1afb201d4a9213dfa0bac9a48a9
brownhr <brownhr314@gmail.com>
2022-07-04 14:50:43 -0400

    Datasets

--- R/co2.R
+++ R/co2.R
@@ -2,8 +2,22 @@ library(tidyverse)
 library(zoo)
 library(lubridate)
 
+maunaloa_raw <- read_csv(
+  "data/co2_weekly_mlo.csv",
+  col_types = cols(
+    ndays = col_skip(),
+    `1 year ago` = col_skip(),
+    `10 years ago` = col_skip(),
+    `increase since 1800` = col_skip()
+  ),
+  skip = 47
+) %>% 
+  mutate(time = lubridate::date_decimal(decimal) %>% as.Date()) %>% 
+  select(time, average)
 
-maunaloa <- read_csv(
+write_rds(maunaloa_raw, "data/maunaloa_raw.Rds")
+
+maunaloa_raw <- read_csv(
   "data/co2_weekly_mlo.csv",
   col_types = cols(
     ndays = col_skip(),
@@ -15,19 +29,15 @@ maunaloa <- read_csv(
 ) %>% 
   mutate(time = lubridate::date_decimal(decimal) %>% as.Date()) %>% 
   select(time, average) %>% 
-  naniar::replace_with_na_all(condition = ~.x < 0) %>% 
+  naniar::replace_with_na_all(condition = ~.x < 0) %>%
   tsbox::ts_ts() %>% 
   zoo::na.approx()
 
 write_rds(maunaloa, "data/maunaloa.Rds")
 
-# co2 <- read_csv("data/co2_weekly_mlo.csv")[c(-(1:47)),] #%>% 
-#   rowwise() %>% 
-#   mutate(
-#     time = as.yearmon(paste(month, year), "%m %Y") %>% as.Date()
-#   ) %>% 
-#   select(
-#     time, average
-#   ) %>% ts_ts()
-# 
-# write_rds(co2, "data/co2_data.Rds")
+
+maunaloa <- readRDS("data/maunaloa.Rds")
+
+maunaloa_missing <- maunaloa
+maunaloa_missing[sample(length(maunaloa_missing),100)] <- NA
+saveRDS(maunaloa_missing, "data/maunaloa_missing.Rds")
