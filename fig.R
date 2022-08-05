71f06caf5424cb049770f22d8e885c1c89443740
brownhr <brownhr314@gmail.com>
2022-08-05 18:05:01 -0400

    Fig

---
+++ fig.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(zoo)
+library(lubridate)
+
+ftse_zoo <- EuStockMarkets[, "FTSE"] %>%
+  as.zoo()
+
+ftse <- data.frame(
+  Date = index(ftse_zoo),
+  Price = coredata(ftse_zoo)
+)
+
+# Rolling Window
+ftse %>%
+  ggplot(aes(x = Date, y = Price)) +
+  geom_line() +
+  geom_hline(
+    yintercept = mean(ftse$Price),
+    color = "blue",
+    linetype = "dashed"
+  ) +
+  theme_light()
+ggsave(filename = "fig/rolling-window-3.png")
+
+
+ftse_rollmean <- ftse_zoo %>%
+  rollmean(
+    k = 30,
+    align = "right"
+  )
+ftse_rollmean2 <- data.frame(
+  Date = index(ftse_rollmean),
+  Price = coredata(ftse_rollmean)
+)
+
+ftse %>%
+  ggplot(aes(x = Date, y = Price)) +
+  geom_line() +
+  geom_line(
+    data = ftse_rollmean2, aes(x = Date, y = Price),
+    color = "red",
+    size = 1
+  ) + 
+  theme_light()
+ggsave(filename = "fig/rolling-window-4.png")
