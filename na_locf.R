d046b4f7863390b9597638ab8a2742b16e90bc80
brownhr <brownhr314@gmail.com>
2022-09-22 15:19:46 -0400

    Create na_locf.R

---
+++ na_locf.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(xts)
+set.seed(1234)
+
+scores <-
+  c((round(100 * runif(
+    n = 24, min = 0.75, max = .85
+  ), 2) + seq(
+    from = 0,
+    to = 15,
+    length.out = 24
+  )),
+  rep(NA, 12)
+  )
+
+index <- seq.Date(from = as_date('2005-01-01'), by = 'month', along.with = scores)
+
+monthly_scores <- zoo(scores, index)
+autoplot(monthly_scores) + 
+  theme_light() +
+  labs(y = "Score",
+       title = "Monthly Test Scores")
+
+scores_locf <- na.locf(monthly_scores)
+
+autoplot(scores_locf) + 
+  theme_light() +
+  labs(y = "Score",
+       title = "Monthly Test Scores (LOCF)")
