42f7078c82ac4bc8cb5c11b34197d315ae8ad5af
brownhr <brownhr314@gmail.com>
2022-09-26 17:03:16 -0400

    Create stocks.R

---
+++ stocks.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(lubridate)
+library(xts)
+
+dow_jones <- read_csv("data/PerformanceGraphExport.csv") %>% 
+  mutate(
+    Date = parse_date_time(Date, "%m/%d/%y")
+  )
+
+dow_jones <- zoo(dow_jones$Price, dow_jones$Date)
+index(dow_jones) <- decimal_date(index(dow_jones))
+
+write_rds(dow_jones, "data/dowjones.rds")
+
+dow_jones %>% 
+  window(start = 2019,
+         end = 2021) %>% 
+  autoplot() + 
+  theme_light() + 
+  labs(
+    y = "Price (USD)",
+    title = "Dow Jones Industrial Average"
+  )
