0b9d69d963a0233dc4abedb3021b90e2aa2a79a3
brownhr <brownhr314@gmail.com>
2022-11-11 16:15:08 -0500

    Update na_fill.R

--- na_fill.R
+++ na_fill.R
@@ -12,7 +12,8 @@ index <- seq.Date(from = as.Date("2017-01-01"), by = "day", along.with = obs)
 observations <- zoo(obs, index)
 autoplot(observations) + 
   theme_light() + 
-  labs(y = "Daily observations") + 
+  labs(y = "Daily observations",
+       title = "Observations, Missing Data Points") + 
   coord_cartesian(ylim = c(0, 6))
 vprint(head(observations))
 table(observations, useNA = 'ifany')
@@ -21,5 +22,6 @@ observations_fill <- na.fill(observations,
                              fill = 0)
 autoplot(observations_fill) + 
   theme_light() + 
-  labs(y = "Daily observations") + 
+  labs(y = "Daily observations",
+       title = "Daily Observations, Filled") + 
   coord_cartesian(ylim = c(0, 6))
